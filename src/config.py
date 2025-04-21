1f7b519add61589bee6fdbc2e91c58cbee737b86
용훈 김 <kimyonghoon1114@gmail.com>
2025-04-22 00:41:44 +0900

    merge

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
-companyCode = "AAPL"
+companyCode = "TSLA"
 sequenceLength = 40
-Date = "2024-06-20"
+Date = "2026-04-20"
 data_columns = ["stock_close", "rate_close", "nasdaq_close", "Revenue", "NetIncome", "TotalAssets", "RSI", "gold_close"]
 dataNumber = 8
